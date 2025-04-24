70d4b389422f101cdcd07110e76f3c1d272c944e
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-24 15:13:13 +0530

    Data Types

---
+++ Data Types/DataType.java
@@ -0,0 +1,68 @@
+package LearningJava;
+
+public class DataType {
+
+	public static void main(String[] args) {
+	
+		// Numeric Data Type
+		
+		int a=300;
+		System.out.println(a);
+		
+		byte b=100;
+		System.out.println(b);
+		
+		short s= 256;
+		System.out.println(s);
+		
+		long l= 1234567890;
+		System.out.println(l);
+		
+		// for long data type if we need to add long value more than 10 digits we must end it with Literal  L or l
+		
+		long m =1234567890123L;
+		System.out.println(m);
+		
+		//Decimal Data Types
+		
+		float  f=12.1F;
+		//If float with decimal value gives error add literal F or f
+		System.out.println(f);
+		
+		double d= 555.555;
+		System.out.println(d);
+		
+				
+		int x=(int)10.5;
+		System.out.println(x);
+
+			
+		
+		double z=100;
+		System.out.println(z);
+		
+		//char data type
+		
+		char e='c';
+		System.out.println(e);
+		
+		
+		char n='5';
+		System.out.println(n);
+		
+		//String 
+		
+		String y="Learn Java";
+		System.out.println(y);
+		
+		//Boolean
+		
+	      boolean h=true;
+	      System.out.println(h);
+	      
+	      
+		
+				
+	}
+
+}
