a0f6da2c14afefa59674089ec905ac3197404d36
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-24 13:42:52 +0530

    Java Variables

---
+++ Java Variables/JavaVariables.java
@@ -0,0 +1,32 @@
+package LearningJava;
+
+public class JavaVariables {
+
+	public static void main(String[] args) {
+		
+		int a;
+		a=6;
+		System.out.println(a);
+		
+		double b;
+		b=7.5;
+		System.out.println(b);
+		
+		
+		char c;
+		c='a';
+		System.out.println(c);
+		
+		
+		float d;
+		d=7;
+		System.out.println(d);
+		
+		System.out.println(a+b+d);
+		System.out.println(a+b+c+d);
+		System.out.println("a"+"b"+"c"+"d");
+		
+
+	}
+
+}
