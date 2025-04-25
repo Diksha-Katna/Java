5dc7182dad5597929fcac449ddd1a0de4130f51b
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-25 18:04:15 +0530

    Sum Of  Numbers

---
+++ Sum Of Numbers/Sum_of_Numbers.java
@@ -0,0 +1,16 @@
+package LearningJava;
+
+public class Sum_of_Numbers {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		int i;
+		for(i=1;i<=10;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+	
+}
