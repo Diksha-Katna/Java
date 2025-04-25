f377c70d74bcbce6e1e6d35ce389e7038b065538
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-25 16:32:56 +0530

    Print Numbers

---
+++ Print Numbers/Print_Numbers.java
@@ -0,0 +1,15 @@
+package LearningJava;
+
+public class Print_Numbers {
+
+	public static void main(String[] args) {
+		
+		int i;
+		for(i=1;i<=100;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
