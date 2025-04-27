d3e0068b1dcf8604b5c159efcbc3d02d9e41d372
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-27 19:40:11 +0530

    Simple Array

---
+++ Simple Array/Array.java
@@ -0,0 +1,19 @@
+package LearningJava;
+
+public class Array {
+
+	public static void main(String[] args) {
+		
+		int a[]=new int[3];
+		a[0]=1;
+	    a[1]=2;
+	    a[2]=3;
+	    System.out.println ("The value at a[2] is :" +a[2]);
+		
+
+		
+		
+
+	}
+
+}
