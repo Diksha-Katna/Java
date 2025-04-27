55a951e36403c49fc739cc786ff7e4781980a5b2
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-27 20:36:35 +0530

    Two Dimensional Array

---
+++ Two Dimensional Array/Two_DimensionalArray.java
@@ -0,0 +1,14 @@
+package LearningJava;
+
+public class Two_DimensionalArray {
+
+	public static void main(String[] args) {
+
+		
+		int a[][]= {{1,2},{6,3},{8,10}};
+		System.out.println(a[1][1]);
+	
+		
+
+}
+}
