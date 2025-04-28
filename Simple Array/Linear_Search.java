374a85457d2fa555ff454407dbd9c4d57b01e79c
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-28 15:54:40 +0530

    Linear Search

---
+++ Simple Array/Linear_Search.java
@@ -0,0 +1,34 @@
+package LearningJava;
+
+
+public class Linear_Search {
+
+	public static void main(String[] args) {
+
+		
+		int a[]= {20,50,24,67,70};
+		int item=50;
+		for(int i=0;i<=4;i++)
+		
+		{
+			if(item==a[i])
+			{
+				System.out.println("The item is found at position " +i);
+				  break;
+			}
+			
+		
+			}
+			
+			
+		}
+		
+		
+		
+		
+		}
+		
+		
+	
+
+
