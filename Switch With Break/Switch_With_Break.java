11365de0351f151d2a378bd319d34fcff27d64ff
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-04-30 14:07:59 +0530

    Switch With Break

---
+++ Switch With Break/Switch_With_Break.java
@@ -0,0 +1,46 @@
+package LearningJava;
+
+public class Switch_With_Break {
+
+	public static void main(String[] args) {
+
+	
+
+		
+				
+
+				   int c=1;
+				   switch(c)
+				
+				{
+				
+							case 1:
+								System.out.println("one ");
+							    break;
+								
+								
+							case 2:
+								
+								System.out.println("two ");
+								break;
+								
+								
+								 
+							case 3:
+								System.out.println("Three");
+								break;
+								
+							
+												
+								
+								default:
+									System.out.println("This is default ");
+				}
+			}
+
+		
+
+
+	}
+
+
