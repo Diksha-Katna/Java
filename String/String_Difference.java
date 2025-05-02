68c556d89041471937a3eb6e818860ca97e62b61
Diksha-Katna <dikshakatnagenuine@gmail.com>
2025-05-02 11:05:49 +0530

    String Difference

---
+++ String/String_Difference.java
@@ -0,0 +1,21 @@
+package LearningJava;
+
+public class String_Difference {
+
+	public static void main(String[] args) {
+		
+		String st="Hello";		
+        st.concat("World");        
+        System.out.println(st);
+        
+        
+        StringBuilder s=new StringBuilder("Welcome");
+        s.append("Java");
+        System.out.println(s);
+        
+        
+      
+
+	}
+
+}
