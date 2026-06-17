e45209fa8db9016d299e100ca991b4da36227055
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-17 14:05:12 +0530

    practice

---
+++ Practice/practice7.java
@@ -0,0 +1,15 @@
+package LearningJava;
+
+public class practice7 {
+
+	public static void main(String[] args) {
+
+		int a=5,i,b;
+		for(i=1;i<=10;i++)
+		{
+			b=a*i;
+			System.out.println(b);
+		}
+	}
+
+}
