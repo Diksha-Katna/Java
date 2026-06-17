e9f3d9bcc518e9171f5a1268a520f2088c19da50
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-17 14:14:49 +0530

    practice

---
+++ Practice/practice9.java
@@ -0,0 +1,18 @@
+package LearningJava;
+
+public class practice9 {
+
+	public static void main(String[] args) {
+      
+		int a=5,i=1,b;
+		do
+		{
+			b=a*i;
+			System.out.println(b);
+			i++;
+		}
+		
+		while(i<=10);
+	}
+
+}
