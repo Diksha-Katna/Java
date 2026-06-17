390ccd38bc334eed19ddc1042cda23b57a03ef96
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-17 13:43:04 +0530

    practice 4

---
+++ Practice/practice4.java
@@ -0,0 +1,16 @@
+package LearningJava;
+
+public class practice4 {
+
+	public static void main(String[] args) {
+
+		int age=18;
+		
+		if(age==18)
+		{
+			System.out.println("You are elligibe for the vote");
+			
+		}
+	}
+
+}
