3c1d6ae33aa9bb8b95258b3e2a6f20f2dbd30f5d
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-17 14:09:34 +0530

    practice

---
+++ Practice/practice8.java
@@ -0,0 +1,16 @@
+package LearningJava;
+
+public class practice8 {
+
+	public static void main(String[] args) {
+
+		int a=5,i=1,b;
+		while(i<=10)
+		{
+			b=a*i;
+			System.out.println(b);
+			i++;
+		}
+	}
+
+}
