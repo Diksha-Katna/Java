b7b2accdf20302fd674d81fc8564c0dddb4ca5f0
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-17 13:40:01 +0530

    practice

---
+++ Practice/practice3.java
@@ -0,0 +1,41 @@
+package LearningJava;
+
+public class practice3 {
+
+	public static void main(String[] args) {
+
+		byte b=12;
+		System.out.println("The byte data type is:"+b);
+		
+		short s=124;
+		System.out.println("The short data type is:"+s);
+		
+		int i=80;
+		long l=1234567890L;
+		System.out.println(i);
+		System.out.println(l);
+		long m=1234567890;
+		System.out.println(m);
+		
+		
+		
+		float fs=123.23F;
+		System.out.println(fs);
+		double d=123.20;
+		System.out.println(d);
+		
+		
+		
+		char ch='c';
+		System.out.println(ch);
+		
+		String str="Hello Diksha";
+		System.out.println(str);	
+		
+		boolean bl=true;
+		System.out.println(bl);
+				
+				
+	}
+
+}
