516724ae2b353065fdfd33e5864ed6c3180eba19
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-17 14:34:24 +0530

    practice

---
+++ Practice/practice10.java
@@ -0,0 +1,23 @@
+package LearningJava;
+
+public class practice10 {
+
+	public static void main(String[] args) {
+		
+		char c='d';
+		switch(c) 
+		{
+		case 'a':System.out.println("this is case a");
+		case 'd':System.out.println("this is case c");
+		default: System.out.println("this is default");
+		}
+		
+		
+		
+		
+	
+			
+			
+	}
+
+}
