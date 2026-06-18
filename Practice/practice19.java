131836f1c114c6504eb70e0c8f11cf44e9917285
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-18 12:06:00 +0530

    practice19

---
+++ Practice/practice19.java
@@ -0,0 +1,22 @@
+package LearningJava;
+
+public class practice19 {
+
+	public static void main(String[] args) {
+
+		int i,count=0;
+		
+		for(i=1;i<=5;i++)
+		{
+			if(i%2!=0)
+			{
+				count++;
+			}
+			
+		
+		}
+		
+		System.out.println(count);
+	}
+
+}
