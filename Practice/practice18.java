3b46f3aca5ea7b25c5d345237664df1aa6a79e84
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-18 12:02:41 +0530

    practice

---
+++ Practice/practice18.java
@@ -0,0 +1,22 @@
+package LearningJava;
+
+public class practice18 {
+
+	public static void main(String[] args) {
+
+		int i,sum=0;
+		
+		for(i=1;i<=5;i++)
+		{
+			if(i%2!=0)
+			{
+				sum=sum+i;
+			}
+			
+		
+		}
+		
+		System.out.println(sum);
+	}
+
+}
