90a9d18122b2a83f5b96d758c12c5d63765c34e1
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-18 10:48:00 +0530

    practice

---
+++ Practice/practice11.java
@@ -0,0 +1,25 @@
+package LearningJava;
+
+public class practice11 {
+
+	public static void main(String[] args) {
+
+		
+		char ch='d';
+		switch(ch) {
+		case 'a': System.out.println("This is first case a");
+		
+		case 'b': System.out.println("This is second case b");
+		
+		case 'd': System.out.println("This is third case d");
+		break;
+		
+		
+		default:
+			
+			System.out.println("This is default case ");
+		
+		}
+	}
+
+}
