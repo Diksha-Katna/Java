7f67b1d4323a63cc2c89548837438894926be148
Diksha-Katna <dikshakatnagenuine@gmail.com>
2026-06-18 11:08:12 +0530

    practice13

---
+++ Practice/practice13.java
@@ -0,0 +1,17 @@
+package LearningJava;
+
+public class practice13 {
+
+	public static void main(String[] args) {
+
+		int i;
+		for(i=1;i<10;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+}
